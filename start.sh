4e2592333d019b4c58d0f788db66a1da0ba0a3cf
hyde2-cpu <mhmmadhidayat2@gmail.com>
2025-07-05 19:20:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-# Opsional: Aktifkan login username/password
+echo "==========[ DEBUG INFO ]=========="
+echo "Tanggal & Waktu     : $(date)"
+echo "Hostname            : $(hostname)"
+echo "User                : $(whoami)"
+echo "Current Directory   : $(pwd)"
+echo "File list           :"
+ls -lah
+
+echo "Environment Vars    :"
+env | grep N8N_
+
+echo "==================================="
+echo "🚀 Starting n8n with basic auth..."
+echo "==================================="
+
+# Aktifkan login username/password (opsional)
 export N8N_BASIC_AUTH_ACTIVE=true
 export N8N_BASIC_AUTH_USER=${N8N_BASIC_AUTH_USER}
 export N8N_BASIC_AUTH_PASSWORD=${N8N_BASIC_AUTH_PASSWORD}
