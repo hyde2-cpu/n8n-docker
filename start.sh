4cd63a7770a89aad0cc9bf0ede2546437c98ee8c
hyde2-cpu <mhmmadhidayat2@gmail.com>
2025-07-05 19:13:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
+# Opsional: Aktifkan login username/password
 export N8N_BASIC_AUTH_ACTIVE=true
-export N8N_BASIC_AUTH_USER=$N8N_BASIC_AUTH_USER
-export N8N_BASIC_AUTH_PASSWORD=$N8N_BASIC_AUTH_PASSWORD
-export WEBHOOK_URL=$WEBHOOK_URL
+export N8N_BASIC_AUTH_USER=${N8N_BASIC_AUTH_USER}
+export N8N_BASIC_AUTH_PASSWORD=${N8N_BASIC_AUTH_PASSWORD}
 
+# Jalankan n8n
 n8n start
