55c3cc92597e6f85a4cff862ebbf15c98fbd7856
hyde2-cpu <mhmmadhidayat2@gmail.com>
2025-07-05 19:28:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,5 +20,8 @@ export N8N_BASIC_AUTH_ACTIVE=true
 export N8N_BASIC_AUTH_USER=${N8N_BASIC_AUTH_USER}
 export N8N_BASIC_AUTH_PASSWORD=${N8N_BASIC_AUTH_PASSWORD}
 
+echo "n8n path: $(which n8n)"
+n8n --version
+
 # Jalankan n8n
 n8n start
